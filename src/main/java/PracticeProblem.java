062827500e3555c2ace12a1d21b872a433d51417
raymond-zeng-ics4u1-03-25 <raymond.zeng4@student.tdsb.on.ca>
2025-04-02 18:47:02 +0000

    update files

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,60 @@
+import java.io.*;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
-	
+	public static String readFile(String filename){
+		FileReader inputStream = null;
+		String result = "";
+		try{
+			inputStream = new FileReader(filename);
+			int c;
+			while((c = inputStream.read()) != -1){
+				result += (char)(c);
+			}
+		}
+		catch(IOException e){
+			System.out.println(e);
+		}
+		finally{
+			try{
+				if(inputStream != null){
+					inputStream.close();
+				}
+			}
+			catch(IOException e){
+				System.out.println(e);
+			}
+		}
+		return result;
+	}
 
+	public static String backwardsReadFile(String filename){
+		FileReader inputStream = null;
+		String result = "";
+		try{
+			inputStream = new FileReader(filename);
+			int c;
+			while((c = inputStream.read()) != -1){
+				result = (char)(c) + result;
+			}
+		}
+		catch(IOException e){
+			System.out.println(e);
+		}
+		finally{
+			try{
+				if(inputStream != null){
+					inputStream.close();
+				}
+			}
+			catch(IOException e){
+				System.out.println(e);
+			}
+		}
+		return result;
+	}
 }
